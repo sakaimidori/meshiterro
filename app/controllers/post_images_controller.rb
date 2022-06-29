f8cac2273eb1fd3d196d1fd4d6defc632b7c0cbe
Your Name <ec2-user@ip-172-31-5-134.ec2.internal>
2022-06-29 17:51:58 +0000

    feat:コメント機能、いいね機能、バリデーション機能

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -5,9 +5,12 @@ class PostImagesController < ApplicationController
 
   def create
     @post_image= PostImage.new(post_image_params)
-    @post_image.user_id =current_user.id
-    @post_image.save
-    redirect_to  post_images_path
+    @post_image.user_id = current_user.id
+    if @post_image.save
+      redirect_to  post_images_path
+    else
+      render:new
+    end
   end
 
   def index
@@ -16,6 +19,7 @@ class PostImagesController < ApplicationController
 
   def show
     @post_image= PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
 
   def destroy
@@ -24,11 +28,11 @@ class PostImagesController < ApplicationController
     redirect_to post_images_path
   end
 
-#ストロングパラメータ
-private
-
-def post_image_params
-  params.require(:post_image).permit(:shop_name,:image,:caption)
-end
+  #ストロングパラメータ
+  private
+  
+  def post_image_params
+    params.require(:post_image).permit(:shop_name,:image,:caption)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   resources:users,only:[:show, :edit, :update]
   
   resources:post_images,only:[:new, :create, :index, :show, :destroy]do
-    resources:post_comments,only:[:create] 
+    resource:favorites,only:[:create, :destroy]
+    resources:post_comments,only:[:create, :destroy] 
   end
+  
   root to: "homes#top"
   get 'homes/about' => 'homes#about',as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
